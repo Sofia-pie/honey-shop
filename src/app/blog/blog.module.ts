6e97ed4aa6080ef157e6eb8981ff5d048ec61736
Sofiia_Piesnia <sophia.piesnia@gmail.com>
2022-10-25 21:37:38 +0300

    add blog-item component

--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { BlogRoutingModule } from './blog-routing.module';
 import { BlogComponent } from './blog.component';
+import { BlogItemComponent } from './blog-item/blog-item.component';
 
 
 @NgModule({
   declarations: [
-    BlogComponent
+    BlogComponent,
+    BlogItemComponent
   ],
   imports: [
     CommonModule,
