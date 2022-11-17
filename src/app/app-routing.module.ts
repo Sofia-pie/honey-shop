2ac4dbef2e1be34270123a96590544d4444eb2ff
Sofiia_Piesnia <sophia.piesnia@gmail.com>
2022-11-17 14:14:05 +0200

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
 import { MainPageComponent } from './main-page/main-page.component';
 
 const routes: Routes = [
   { path: 'main-page', component: MainPageComponent },
+
   {
     path: 'products',
     loadChildren: () =>
       import('./products/products.module').then((m) => m.ProductsModule),
   },
+  {
+    path: 'blog',
+    loadChildren: () => import('./blog/blog.module').then((m) => m.BlogModule),
+  },
+  { path: 'cart', component: CartComponent },
+
   { path: '', pathMatch: 'full', redirectTo: 'main-page' },
-  { path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) },
-  { path: 'products', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule) },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartModule } from './cart/cart.module';
 import { CoreModule } from './core/core.module';
 import { MainPageModule } from './main-page/main-page.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, MainPageModule, CoreModule],
+  imports: [
+    BrowserModule,
+    MainPageModule,
+    CoreModule,
+    AppRoutingModule,
+    CartModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
