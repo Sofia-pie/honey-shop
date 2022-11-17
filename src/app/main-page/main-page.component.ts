95f806953f50f833fe253ce8981d49053212f8be
Sofiia_Piesnia <sophia.piesnia@gmail.com>
2022-11-17 14:29:16 +0200

    main page

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { take } from 'rxjs/operators';
+import { Blog } from '../core/models/blog';
+import { Product } from '../core/models/product';
+import { BlogService } from '../core/services/blog.service';
+import { ProductService } from '../core/services/product.service';
 
 @Component({
   selector: 'app-main-page',
@@ -6,11 +11,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-page.component.css'],
 })
 export class MainPageComponent implements OnInit {
-  products = [
-    { id: 1, name: 'Мед Липовий', price: 234 },
-    { id: 2, name: 'Мед', price: 234 },
-  ];
-  constructor() {}
+  products: Product[];
+  blogs: Blog[];
+  constructor(
+    private productService: ProductService,
+    private blogService: BlogService
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.productService
+      .getProducts()
+      .subscribe((p) => (this.products = p.slice(0, 4)));
+
+    this.blogService.getBlogs().subscribe((b) => (this.blogs = b.slice(0, 3)));
+  }
+
+  buy(pr: any) {
+    console.log('Buy :' + pr.name);
+  }
 }
--- src/app/main-page/main-page.module.ts
+++ src/app/main-page/main-page.module.ts
@@ -3,10 +3,10 @@ import { CommonModule } from '@angular/common';
 import { CoreModule } from '../core/core.module';
 import { MainPageComponent } from './main-page.component';
 import { SharedModule } from '../shared/shared.module';
-
+import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [MainPageComponent],
-  imports: [CommonModule, CoreModule, SharedModule],
+  imports: [CommonModule, CoreModule, SharedModule, RouterModule],
   exports: [MainPageComponent],
 })
 export class MainPageModule {}
