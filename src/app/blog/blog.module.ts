2b33d875e177cf8029364d81f084e7b3a1740cb4
Sofiia_Piesnia <sophia.piesnia@gmail.com>
2022-11-19 16:16:07 +0200

    add styles

--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -4,16 +4,10 @@ import { CommonModule } from '@angular/common';
 import { BlogRoutingModule } from './blog-routing.module';
 import { BlogComponent } from './blog.component';
 import { BlogItemComponent } from './blog-item/blog-item.component';
-
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [
-    BlogComponent,
-    BlogItemComponent
-  ],
-  imports: [
-    CommonModule,
-    BlogRoutingModule
-  ]
+  declarations: [BlogComponent, BlogItemComponent],
+  imports: [CommonModule, BlogRoutingModule, SharedModule],
 })
-export class BlogModule { }
+export class BlogModule {}
